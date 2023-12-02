50d5624b32910014b28968e9f78213328f124469
ThomasGysemans <support@sciencesky.fr>
2023-12-02 18:19:39 +0100

    Complete first challenge

--- challenge-1/main.cpp
+++ challenge-1/main.cpp
@@ -2,36 +2,12 @@
 #include <string> 
 #include <regex>
 #include <fstream>
+#include <map>
 using namespace std;
 
-static const regex digit_regex(R"(\d)");
+static map<string, int> DIGITS{{"zero", 0}, {"one", 1}, {"two", 2}, {"three", 3}, {"four", 4}, {"five", 5}, {"six", 6}, {"seven", 7}, {"eight", 8}, {"nine", 9}};
 
-// Firt solution
-int get_calibration_value_from(const string& str) {
-  char first_digit = '0';
-  char second_digit = '0';
-  for (auto iter = str.begin(); iter != str.end(); ++iter) {
-    if (regex_search(string(1, *iter), digit_regex)) {
-      first_digit = (*iter);
-      break;
-    }
-  }
-  for (auto iter = str.end(); iter != str.begin() - 1; --iter) {
-    if (regex_search(string(1, *iter), digit_regex)) {
-      second_digit = (*iter);
-      break;
-    }
-  }
-  // Let's take for example 8 and 5.
-  // Our goal is to combine both numbers into a single integer.
-  // If first_digit is 8, then 8 * 10 is 80.
-  // We add to this result 5, giving us 85.
-  // Note that substracting '0' from the ASCII value of first_digit and second_digit
-  // allows us to simply convert the character into a number.
-  return (first_digit - '0') * 10 + (second_digit - '0');
-}
-
-// Second solution
+// First solution (the first part of the first challenge).
 int get_calibration(const string& str) {
   // select as less characters as possible until the first digit,
   // then select as many as possible until the next digit (so select the last one).
@@ -46,14 +22,34 @@ int get_calibration(const string& str) {
   }
 }
 
+// Full day-1 challenge
+int get_full_calibration(const string& str) {
+  const regex reg("(?:^.*?(\\d|(?:one|two|three|four|five|six|seven|eight|nine|zero)).*(\\d|(?:one|two|three|four|five|six|seven|eight|nine|zero)).*$)|(?:\\d|(?:one|two|three|four|five|six|seven|eight|nine|zero))");
+  const auto iter = sregex_iterator(str.begin(), str.end(), reg);
+  const smatch match = *iter;
+  if (match[2].str().empty()) { // meaning there was only one number
+    if (match.str().length() == 1) { // one character means a digit, otherwise it's a string (like "one" for example)
+      return stoi(string(2, match.str().at(0))); // this single digit is duplicated once.
+    } else {
+      const int n = DIGITS[match.str()]; // "one" transformed into 1, "two" transformed into 2, etc.
+      return n * 10 + n;
+    }
+  } else {
+    const string first = match[1].str();
+    const string second = match[2].str();
+    const int first_digit = first.length() == 1 ? stoi(first) : DIGITS[first];
+    const int second_digit = second.length() == 1 ? stoi(second) : DIGITS[second];
+    return first_digit * 10 + second_digit;
+  }
+}
+
 int main() {
   int sum = 0;
 
   string line;
   ifstream file("./challenge-1/input.txt"); // the code will simply return 0 if there is no input file
   while (getline(file, line)) {
-    int i = get_calibration(line);
-    sum += i;
+    sum += get_full_calibration(line);
   }
   file.close();
 
